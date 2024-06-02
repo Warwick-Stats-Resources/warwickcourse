fcdbd680c5204a55e8db24f5c952231c709094c8
EllaKaye <ella.kaye@gmail.com>
2024-06-02 17:36:54 +0100

    use here::here instead of library(here)

--- R/sessions.R
+++ R/sessions.R
@@ -1,10 +1,8 @@
-library(here)
-
 create_session <- function(name) {
   
   # new slides
-  from_slides <- here("slides", "00-template")
-  to_slides <- here("slides", name)
+  from_slides <- here::here("slides", "00-template")
+  to_slides <- here::here("slides", name)
 
   dir.create(to_slides)
   file.copy(list.files(from_slides, full.names = TRUE),
@@ -12,22 +10,22 @@ create_session <- function(name) {
             recursive = TRUE)
   
   # new content
-  from_content <- here("content", "00-template")
-  to_content <- here("content", name)
+  from_content <- here::here("content", "00-template")
+  to_content <- here::here("content", name)
   dir.create(to_content)
   file.copy(list.files(from_content, full.names = TRUE),
             to_content,
             recursive = TRUE)
   
-  index <- readLines(here("content", name, "index.qmd"))
+  index <- readLines(here::here("content", name, "index.qmd"))
   index_new_links <- gsub("UPDATE-DIR-NAME", name, index)
-  writeLines(index_new_links, here("content", name, "index.qmd"))
+  writeLines(index_new_links, here::here("content", name, "index.qmd"))
 
 }
 
 delete_session <- function(name) {
-  content <- here("content", name)
-  slides <- here("slides", name)
+  content <- here::here("content", name)
+  slides <- here::here("slides", name)
   
   unlink(content, recursive = TRUE)
   unlink(slides, recursive = TRUE)
